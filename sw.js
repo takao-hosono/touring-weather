0b1bec08880033916e867b007f5a401c807ea32e
takao-hosono <takao_hosono@yahoo.co.jp>
2026-09-16 17:26:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const C = "tw-v04";
+self.addEventListener("install", (e) =>
+  e.waitUntil(
+    caches
+      .open(C)
+      .then((c) => c.addAll(["./", "./index.html", "./manifest.json"])),
+  ),
+);
+self.addEventListener("activate", (e) =>
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((k) => k !== C).map((k) => caches.delete(k))),
+      ),
+  ),
+);
+self.addEventListener("fetch", (e) =>
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))),
+);
