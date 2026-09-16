293ad8491b600ff47a659b3853159b0c89e1c4dd
takao-hosono <takao_hosono@yahoo.co.jp>
2026-09-16 17:57:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const C = "tw-v04";
+const C = "tw-v05";
 self.addEventListener("install", (e) =>
   e.waitUntil(
     caches
       .open(C)
-      .then((c) => c.addAll(["./", "./index.html", "./manifest.json"])),
+      .then((c) => c.addAll(["./index.html", "./manifest.json"]))
+      .then(() => self.skipWaiting()),
   ),
 );
 self.addEventListener("activate", (e) =>
@@ -12,9 +13,32 @@ self.addEventListener("activate", (e) =>
       .keys()
       .then((keys) =>
         Promise.all(keys.filter((k) => k !== C).map((k) => caches.delete(k))),
-      ),
+      )
+      .then(() => self.clients.claim()),
   ),
 );
-self.addEventListener("fetch", (e) =>
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))),
-);
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request)
+        .then((r) => {
+          const copy = r.clone();
+          caches.open(C).then((c) => c.put("./index.html", copy));
+          return r;
+        })
+        .catch(() => caches.match("./index.html")),
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(
+      (cached) =>
+        cached ||
+        fetch(e.request).then((r) => {
+          if (e.request.method === "GET" && new URL(e.request.url).origin === location.origin)
+            caches.open(C).then((c) => c.put(e.request, r.clone()));
+          return r;
+        }),
+    ),
+  );
+});
